9539d7da71dc0e75cff66521a814954313b4bfa4
Linggg <lingyu4113@gmail.com>
2019-07-11 20:57:24 +0800

    11

---
+++ Project1/Project1/2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int main() 
+{
+	float a, b;
+	int num;//ÊýÁ¿
+	cin >> a >> b;
+	a = a + b * 0.1;
+	num = a / 1.9;
+	cout << num;
+	return 0;
+}
