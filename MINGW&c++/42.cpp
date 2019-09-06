37eaec48d3acc5af0829db29d2f940285860d1ec
Linggg <lingyu4113@gmail.com>
2019-09-06 23:45:49 +0800

    6

--- MINGW&c++/42.cpp
+++ MINGW&c++/42.cpp
@@ -1,39 +1,50 @@
 //p1002
 #include<iostream>
-#include<algorithm>
 using namespace std;
-
-const int N=30;
+const int N=40;
+long long dp[N][N];
 long long a[N][N];
-long long n,m,mx,my;
+int dx[9]={0,-1,1,-1,1,-2,-2,2,2};
+int dy[9]={0,-2,-2,2,2,-1,1,-1,1};
+int n,m,mx,my;
+
 int main()
 {
     cin>>n>>m>>mx>>my;
-    for(int i=0;i<=n;i++)
-        for(int j=0;j<=m;j++)
-            a[i][j]=1;
-    a[mx][my]=0;
-    a[mx+2][my-1]=0;
-    a[mx+2][my+1]=0;
-    a[mx-2][my-1]=0;
-    a[mx-2][my+1]=0;
-    a[mx-1][my+2]=0;
-    a[mx+1][my+2]=0;
-    a[mx-1][my-2]=0;
-    a[mx+1][my-2]=0;
+    n++;m++;mx++;my++;
+    for(int i=0;i<=8;i++)
+       { dp[mx+dx[i]][my+dy[i]]=-1;
+        cout<<mx+dx[i]<<","<<my+dy[i]<<endl;
+       }
+    // dp[0][1]=1;
+    // dp[1][0]=1;
+
+    dp[0][1]=1;
     for(int i=1;i<=n;i++)
         for(int j=1;j<=m;j++)
+        {   
+            a[i][j]=dp[i][j-1];
+            if(dp[i][j]==-1)
+                {dp[i][j]=0;
+                    continue;
+                    }
+            else
             {
-                if(a[i][j]!=0)
-                    a[i][j]=a[i-1][j]+a[i][j-1];
+                if(i==1&&j>1)
+                    dp[i][j]=dp[i][j-1];
+                else if(i>1&&j==1)
+                    dp[i][j]=dp[i-1][j];
+                else 
+                    dp[i][j]=dp[i][j-1]+dp[i-1][j];
             }
-
-    cout<<a[n][m];
-
-
-
-
+        }
+        //     for(int i=1;i<=n;i++)
+        // for(int j=1;j<=m;j++)
+        //     cout<<a[i][j];
+    cout<<dp[n][m];
 
     system("pause");
     return 0;
+
+
 }
