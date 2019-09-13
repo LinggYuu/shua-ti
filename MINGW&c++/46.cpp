99869231bc1ffafdb3bd3e7650ec183d0fff25f1
Linggg <lingyu4113@gmail.com>
2019-09-14 00:08:52 +0800

    14

--- MINGW&c++/46.cpp
+++ MINGW&c++/46.cpp
@@ -4,3 +4,26 @@
 #include<algorithm>
 using namespace std;
 
+int main()
+{
+    int i,L,R,num=0;
+    cin>>L>>R;
+    for(i=L;i<=R;i++)
+    {
+        int t=i;
+        while(t>0)
+        {   
+            if(t%10==2)
+            {
+                num++;
+            }
+            t/=10;
+        }
+    }
+    cout<<num;
+
+
+
+    system("pause");
+    return 0;
+}
