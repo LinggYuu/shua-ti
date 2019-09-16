548603c7a4d319d06a2fba7e6b9f8271cd535d52
Linggg <lingyu4113@gmail.com>
2019-09-16 16:55:23 +0800

    16

--- MINGW&c++/49.cpp
+++ MINGW&c++/49.cpp
@@ -1,26 +1,24 @@
+// R24000490
 #include<iostream>
 #include<algorithm>
 using namespace std;
-
-
 
 int N,m;
 int p[30],w[30];
 int dp[30][30100];
 
 
-
 int main()
 {
     cin>>N>>m;
-    for(int i=1;i<=m;i++)
+    for(int i=0;i<m;i++)
         cin>>p[i]>>w[i];
     for(int i=1;i<=m;i++)
         for(int j=0;j<=N;j++)
         {
             dp[i][j]=dp[i-1][j];
-            if(j>=p[i])
-            dp[i][j]=max(dp[i-1][j],(dp[i-1][j-p[i]]+w[i]*p[i]));
+            if(j>=p[i-1])
+            dp[i][j]=max(dp[i-1][j],(dp[i-1][j-p[i-1]]+w[i-1]*p[i-1]));
         }
     cout<<dp[m][N];
 
---
+++ MINGW&c++/50.cpp
@@ -0,0 +1,40 @@
+// p1048
+#include<iostream>
+using namespace std;
+
+int t[101];
+int w[101];
+int dp[101][1010];
+
+
+
+int main()
+{
+    int T,M;
+    cin>>T>>M;
+    for(int i=1;i<=M;i++)
+        cin>>t[i]>>w[i];
+    
+    for(int i=1;i<=M;i++)
+        for(int j=0;j<=T;j++)
+        {
+            dp[i][j]=dp[i-1][j];
+            if(j-t[i]>=0)
+            {
+                dp[i][j]=max(dp[i-1][j],(dp[i-1][j-t[i]]+w[i]));
+            }
+
+        }
+
+    cout<<dp[M][T];
+
+
+
+
+
+
+
+
+    system("pause");
+    return 0;
+}
