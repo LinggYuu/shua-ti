79ec2afa2481071a27fd096b652600560cfb8438
Linggg <lingyu4113@gmail.com>
2019-11-18 14:25:01 +0800

    18

--- MINGW&c++/86.5.cpp
+++ MINGW&c++/86.5.cpp
@@ -1,4 +1,71 @@
 #include<iostream>
 #include<cstring>
+#include<string>
 #include<cstdio>
 using namespace std;
+
+int h[15][15];
+int s[15],l[15];//s存左部，l存长度
+int num,x[30],nn[500];
+int n,k,t,tt=-1;
+int st[15];
+string words;
+void dfs(int c)
+{
+    st[c]=1;
+    for(int i=1;i<=l[c];i++)
+    {
+        if(st[h[c][i]])
+            continue;
+        num++;
+        dfs(h[c][i]);
+    }
+}
+void mul(int a[],int b)
+{
+    for(int i=1;i<=a[0];i++)
+        a[i]*=b;
+    for(int i=1;i<=a[0];i++)
+    {
+        if(a[i]>=10)
+        {
+            a[i+1]+=a[i]/10;
+            a[i]%=10;
+            if(i==a[0]) 
+                a[0]++;
+        }
+    }
+}
+int main()
+{
+    memset(l,0,sizeof(l));
+    cin.tie();
+    x[0]=1;
+    x[1]=1;
+    cin>>words>>k;
+    for(int i=1;i<=k;i++)
+    {
+        int a,b;
+        cin>>a>>b;
+        h[a][++l[a]]=b;
+        if(l[a]==1)
+        {
+            s[++tt]=a;
+        }
+    }
+    for(int i=0;i<words.size();i++)
+    {
+        memset(st,0,sizeof(st));
+        dfs(words[i]-'0');
+        mul(x,num+1);
+        num=0;
+    }
+    for(int i=x[0];i>=1;i--)
+    {
+        cout<<x[i];
+    }
+
+    
+    system("pause");
+    return 0;
+}
--- MINGW&c++/89.cpp
+++ MINGW&c++/89.cpp
@@ -1,39 +1,39 @@
-// p1303
-#include<iostream>
-#include<string>
-#include<vector>
-using namespace std;
+// // p1303
+// #include<iostream>
+// #include<string>
+// #include<vector>
+// using namespace std;
 
-vector<int> mul(vector<int>&a,int b)
-{
-    vector<int>C;
-    int t=0;
-    for(int i=0;i<a.size()||t;i++)
-    {
-        if(i<a.size())
-            t+=a[i]*b;
-        C.push_back(t%10);
-        t/=10;
-    }
-    return C;
-}
+// vector<int> mul(vector<int>&a,int b)
+// {
+//     vector<int>C;
+//     int t=0;
+//     for(int i=0;i<a.size()||t;i++)
+//     {
+//         if(i<a.size())
+//             t+=a[i]*b;
+//         C.push_back(t%10);
+//         t/=10;
+//     }
+//     return C;
+// }
 
 
-int main()
-{
-    string a;
-    int b;//123456,789
-    vector<int>A;
-    cin>>a>>b;
-    for(int i=a.size()-1;i>=0;i--)
-    {
-        A.push_back(a[i]-'0');//逆序存,654321
-    }
-    auto C=mul(A,b);
-    for(int i=C.size()-1;i>=0;i--)
-    {
-        cout<<C[i];
-    }
-    system("pause");
-    return 0;
-}
+// int main()
+// {
+//     string a;
+//     int b;//123456,789
+//     vector<int>A;
+//     cin>>a>>b;
+//     for(int i=a.size()-1;i>=0;i--)
+//     {
+//         A.push_back(a[i]-'0');//逆序存,654321
+//     }
+//     auto C=mul(A,b);
+//     for(int i=C.size()-1;i>=0;i--)
+//     {
+//         cout<<C[i];
+//     }
+//     system("pause");
+//     return 0;
+// }
